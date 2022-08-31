5f3baaab8b91c7d1c5329ec2d5028dcefdf31aa4
Maddox <mcxlewis@gmail.com>
2022-08-31 03:02:04 -0400

    Add css file and basic elements

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 let getComputerChoice = ['Rock', 'Paper', 'Scissors'];
+
+']'
+const random = (answer) => {
+]    let answer = 'rock', 'paper', ;'scissors';
+
+    // Pick answer randomly
+    let str = '';
+    for (let i = 0; i < length; i++) {
+        str += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return str;
+
+};
+
+console.log(random());
