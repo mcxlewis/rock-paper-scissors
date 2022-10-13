d536f6a715c9f765018745683973abf76e84147c
Maddox <mcxlewis@gmail.com>
2022-10-13 15:05:54 -0400

    Establish getComputerChoice function

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
     return 'rock';
-    } 
-    if (randomNumber === 1) {
+    } else if (randomNumber === 1) {
     return 'paper';
-    } 
-    if (randomNumber === 2) {
+    } else { 
     return 'scissors'; 
     }
 }
