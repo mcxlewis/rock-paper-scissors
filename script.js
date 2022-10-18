b58fe569aafefee5d812fc121016c23351fb2a76
Maddox <mcxlewis@gmail.com>
2022-10-18 16:58:44 -0400

    Complete 5-round game logic

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
-
 function getComputerChoice() { // randomizing computer choice
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
     return 'rock';
-    } else if (randomNumber === 1) {
+    } 
+    else if (randomNumber === 1) {
     return 'paper';
-    } else { (randomNumber === 2); 
+    } 
+    else { (randomNumber === 2); 
     return 'scissors'; 
     }
 }
 
+let playWins = 0;
+let compWins = 0;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'paper') {  // lose conditions
-        console.log('you lose! paper beats rock!');
-        return 'you lose! paper beats rock!';
+        console.log('paper beats rock!');
+        compWins++;
+        return 'paper beats rock!';
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock') { 
-        console.log('you lose! rock beats scissors!');
-        return 'you lose! rock beats scissors!';
+        console.log('rock beats scissors!');
+        compWins++;   
+        return 'rock beats scissors!';
     }
    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('you lose! scissors beats paper!');
-        return 'you lose! scissors beats paper!';
+        console.log('scissors beats paper!');
+        compWins++;
+        return 'scissors beat paper!';
     }
-    else if (playerSelection === 'paper' && computerSelection === 'rock') { 
-        console.log('you win! paper beats rock!');
-        return 'you win! paper beats rock!'; 
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {  // win conditions
+        console.log('paper beats rock!');
+        playWins++;
+        return 'paper beats rock!'; 
     }
-    else if (playerSelection === 'rock' && computerSelection === 'scissors') {  // win conditions
-        console.log('you win! rock beats scissors!');
-        return 'you win! rock beats scissors!';
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('rock beats scissors!');
+        playWins++;
+        return 'rock beats scissors!';
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper') { 
-        console.log('you win! scissors beat paper!');
-        return 'you win! scissors beat paper!';
+        console.log('scissors beat paper!');
+        playWins++;
+        return 'scissors beat paper!';
     }
     else {
-        console.log('draw! try again!');
-        return 'draw! try again!'; 
+        console.log('draw!');
+        return 'draw!';
     } 
 }
 
@@ -48,6 +58,21 @@ function game() {
         let userChoice = prompt('rock, paper, or scissors?');
         const compChoice = getComputerChoice();
         playRound(userChoice, compChoice);
+    }
+    if (compWins < playWins) {
+        console.log('----------');
+        console.log('congrats! you won! the score was ' + playWins + ' - ' + compWins);
+        return 'congrats! you won!';
+    }
+    else if (compWins > playWins) {
+        console.log('----------');
+        console.log('bummer! you lost! the score was ' + compWins + ' - ' + playWins);
+        return 'bummer! you lost!';
+    }
+    else {
+        console.log('----------');
+        console.log('draw! play again?');
+        return 'draw! play again?';
     }
 }
 
